9fcb0b3956c9699dbeb61d862d9f0e422b8f0cd3
Samuel Cipriano <samciprianoh@gmail.com>
2022-02-17 22:00:14 -0300

    add fixed menu and shadow in dropdow menu

--- js/script.js
+++ js/script.js
@@ -9,3 +9,23 @@ function openDropdown(event) {
 
 btnDropdown.addEventListener('click', openDropdown)
 dropdown.addEventListener('mouseleave', openDropdown)
+
+
+const header = document.getElementById('js-header')
+
+function fixedMenu() {
+  if (window.pageYOffset > 80) {
+    header.classList.add('fixed-menu')
+    return
+  }
+
+  header.classList.remove('fixed-menu')
+
+}
+
+document.addEventListener('scroll', fixedMenu)
+
+
+
+
+
